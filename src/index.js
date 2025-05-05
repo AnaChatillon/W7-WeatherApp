bf91d1a5bf7c0a370a2ce0f9eaa1e5c3817a50ba
Ana Chatillon <a_chantil@hotmail.com>
2025-05-05 23:19:51 +0100

    Improved HTML & JS with Current Time

--- src/index.js
+++ src/index.js
@@ -5,12 +5,32 @@ function displayTemp(response) {
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windSpeedElement = document.querySelector("#wind-speed");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
 
   cityElement.innerHTML = response.data.city;
+  timeElement.innerHTML = formatDate(date);
   temperatureElement.innerHTML = Math.round(temperature);
   descriptionElement.innerHTML = response.data.condition.description;
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+}
+
+function formatDate(date) {
+  let hours = date.getHours().toString().padStart(2, "0");
+  let minutes = date.getMinutes().toString().padStart(2, "0");
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  return `${day} ${hours}:${minutes},`;
 }
 
 function searchCity(city) {
@@ -30,22 +50,3 @@ let form = document.querySelector("#city-search-form");
 form.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Sintra");
-
-// Show the curruent time (Week day + hour & minutes)
-
-let now = new Date();
-let week = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
-let day = week[now.getDay()];
-let minutes = now.getMinutes().toString().padStart(2, "0");
-let hours = now.getHours().toString().padStart(2, "0");
-
-let currentTime = document.querySelector(".currentTime");
-currentTime.innerHTML = `${day} ${hours}:${minutes}`;
